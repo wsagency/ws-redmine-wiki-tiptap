8b7fc49503393358cad555abe715bc25426e22f1
Kristijan Lukacin <kristijan@lukacin.com>
2026-02-16 08:35:51 +0100

    fix: exclude build/node_modules from repo

--- build/node_modules/@tiptap/extension-table/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { Table } from './table.js'
-
-export * from './table.js'
-export * from './TableView.js'
-export * from './utilities/createColGroup.js'
-export * from './utilities/createTable.js'
-
-export default Table
